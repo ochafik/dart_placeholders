74510efe88d60a62819de438d4de225ea7684ede
Olivier Chafik <olivier.chafik@gmail.com>
2015-04-23 10:26:22 -0700

    Added basic tests

--- lib/placeholders.dart
+++ lib/placeholders.dart
@@ -1,50 +1,51 @@
 library placeholders;
 
-@MirrorsUsed(symbols: const ["calls"], override: "*")
-import 'dart:mirrors';
+import 'package:smoke/smoke.dart' as smoke;
 
-/// TODO(ochafik): Add barback typechecks.
+//@MirrorsUsed(symbols: const ["calls"], override: "*")
+//import 'dart:mirrors';
+
+typedef dynamic _Function1(dynamic target);
+
+/// TODO(ochafik): Add transformer typechecks + rewrites.
 class Placeholder extends Function {
-  final Function _target;
-  final bool _isElvis;
-  Placeholder(this._isElvis, this._target);
+  final _Function1 _chainedCall;
+  final bool _skipsNulls;
+  Placeholder(this._skipsNulls, this._chainedCall);
   
-  // TODO(ochafik): File bug about List.map and al. failing to recognize functional objects as functions.
-  dynamic call(dynamic);
-
-  noSuchMethod(Invocation i) {
-    var n = i.memberName;
-    if (MirrorSystem.getName(n) == "call") {
-      return _target(i.positionalArguments.single);
-    } else {
-      Function invoker;
-      if (i.isGetter) {
-        invoker = (target) => reflect(target).getField(n).reflectee;
-      } else if (i.isSetter) {
-        var value = i.positionalArguments.single;
-        invoker = (target) => reflect(target).setField(n, value);
-      } else {
-        assert(i.isMethod);
-        var positionalArguments = i.positionalArguments;
-        var namedArguments = i.namedArguments;
-        invoker = (target) => reflect(target).invoke(n, positionalArguments, namedArguments).reflectee;
-      }
-      return _map(invoker);
-    }
+  dynamic call(arg) {
+    return _chainedCall(arg);
   }
 
+  noSuchMethod(Invocation i) {
+    final n = i.memberName;
+    _Function1 invoker;
+    if (i.isGetter) {
+      invoker = (target) => smoke.read(target, n);
+    } else if (i.isSetter) {
+      final value = i.positionalArguments.single;
+      invoker = (target) => smoke.write(target, n, value);
+    } else {
+      assert(i.isMethod);
+      final positionalArguments = i.positionalArguments;
+      final namedArguments = i.namedArguments;
+      invoker = (target) => smoke.invoke(target, n, positionalArguments, namedArgs: namedArguments);
+    }
+    return _map(invoker);
+  }
+  
   /// Dirty overrides that return placeholders.
   toString() => _map((v) => v.toString());
   get hashCode => _map((v) => v.hashCode);
   operator==(other) => _map((v) => v == other);
 
-  Placeholder _map(Function transform) =>
-      new Placeholder(_isElvis, (v) {
-        var value = _target(v);
-        return _isElvis && value == null ? null : transform(value);
+  Placeholder _map(_Function1 transform) =>
+      new Placeholder(_skipsNulls, (v) {
+        final value = _chainedCall(v);
+        return _skipsNulls && value == null ? null : transform(value);
       });
 }
 
-var $_ = new Placeholder(false, (v) => v);
-var $__ = new Placeholder(true, (v) => v);
-
+_identity(v) => v;
+var $_ = new Placeholder(false, _identity);
+var $__ = new Placeholder(true, _identity);
---
+++ test/placeholders_test.dart
@@ -0,0 +1,48 @@
+import 'package:unittest/unittest.dart';
+import 'package:Placeholders/placeholders.dart';
+
+class Foo { int i; }
+class Bar { Foo foo; }
+class Baz { Bar bar; }
+
+final _ = $_;
+final __ = $__;
+
+main() {
+  group('Placeholder', () {
+    group('_', () {
+      test('chains calls', () {
+        final list = [new Baz()..bar = (new Bar()..foo = (new Foo()..i = 10))];
+        expect(list.map(_.bar.foo.i), [10]);
+        expect(list.map(_.bar.foo.i.toString()), ["10"]);
+        expect(list.map(_.bar.foo.i == 10), [true]);
+        expect(list.map(_.bar.foo.i.hashCode), [10.hashCode]);
+
+        expect([1, 2, 3, 4].where(_ % 2 == 0), [2, 4]);
+        expect([1, 2, 3, 4].where(_ <= 2), [1, 2]);
+        expect([1, 2, 3, 4].where(_.toString() == "2"), [2]);
+      });
+      test('chokes on nulls', () {
+        expect(() => [null].map(_.bar).toList(), throws);
+      });
+    });
+    group('__', () {
+      test('skips nulls', () {
+        final list = [
+          null,
+          new Baz(),
+          new Baz()..bar = (new Bar()),
+          new Baz()..bar = (new Bar()..foo = (new Foo())),
+          new Baz()..bar = (new Bar()..foo = (new Foo()..i = 10)),
+        ];
+        expect(list.map(__.bar.foo.i), [
+          null,
+          null,
+          null,
+          null,
+          10
+        ]);
+      });
+    });
+  });
+}
